4258c46d1052b02f92d35110a89c4d0c62f58f03
kumaravelb <velkumar498@gmail.com>
2019-11-18 23:01:06 +0530

    commit

---
+++ ProjectSample/src/main/java/com/java/example/StudentExample.java
@@ -0,0 +1,25 @@
+package com.java.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+		List<Student> studentList = new ArrayList<Student>();
+        
+		studentList.add(new Student("Paul", 11, "Economics", 78.9));
+		studentList.add(new Student("Zevin", 12, "Computer Science", 91.2));
+		studentList.add(new Student("Harish", 13, "History", 83.7));
+		studentList.add(new Student("Xiano", 14, "Literature", 71.5));
+		studentList.add(new Student("Soumya", 15, "Economics", 77.5));
+		studentList.add(new Student("Asif", 16, "Mathematics", 89.4));
+		studentList.add(new Student("Nihira", 17, "Computer Science", 84.6));
+		studentList.add(new Student("Mitshu", 18, "History", 73.5));
+		studentList.add(new Student("Vijay", 19, "Mathematics", 92.8));
+		studentList.add(new Student("Harry", 20, "History", 71.9));
+		
+		
+	}
+
+}
